5f7a5925f5df6a2583b9608a5d40b5021d5baede
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-04 12:13:52 -0500

    latest code

---
+++ routes/apiRoutes/departmentRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+
+//get all departments
+router.get('/departments', (req, res) => {
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
