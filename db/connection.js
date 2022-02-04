aa76efeae416a56163d89e45dcae531c33f4b08a
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-04 09:09:05 -0500

    connection script

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'TheNorthRemembers',
+        database: 'company'
+    },
+    console.log('Connected to the company database.')
+);
+
+module.exports = db;
