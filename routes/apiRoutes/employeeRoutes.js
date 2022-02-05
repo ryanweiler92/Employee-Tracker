e5337aa5afcfbc44d619f0dc68e5197d29efde25
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-05 10:23:52 -0500

    employee routes

---
+++ routes/apiRoutes/employeeRoutes.js
@@ -0,0 +1,83 @@
+const inputCheck = require('../../utils/inputCheck');
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+
+//get all employees
+router.get('/employees', (req, res) => {
+    const sql = `SELECT * FROM employee`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//get employee by id
+router.get('/employee/:id', (req, res) => {
+    const sql = `SELECT * FROM employee WHERE ID = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//add employee
+router.post('/employee', ({ body }, res) => {
+    // Data validation
+    // const errors = inputCheck(body, 'name');
+    // if (errors) {
+    //     res.status(400).json({ error: errors });
+    //     return;
+    // }
+    const sql = `INSERT INTO employee (first_name, last_name, role_id) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.role_id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body,
+        changes: result.affectedRows
+      });
+    });
+  });
+
+  //delete an employee
+router.delete('/employee/:id', (req, res) => {
+    const sql = `DELETE FROM employee WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            //checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Employee not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+module.exports = router;
